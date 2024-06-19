d08877d872edf58d608bd13f299615c3afb437db
muzamiluddin mohammed <muzamiluddin.mohammed@audreeinfotech.com>
2024-06-19 12:28:06 +0530

    small_changes

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -5,71 +5,66 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
 })
-export class PortfolioComponent implements OnInit{ 
-  constructor(private fb:FormBuilder){}
+export class PortfolioComponent {
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
-  }
+  formApi: FormGroup = this.fb.group({
+    from_name: '',
+    from_email: '',
+    message: '',
+  });
 
-  formApi:FormGroup = this.fb.group({
-    from_name:'',
-    from_email:'',
-    message:''
-  })
-
- async send(){
-  emailjs.init('kJfS8RR9dekvphXCy'); 
-      let response  = await emailjs.send("service_pwe1eny","template_r7ay4xe",{
-        from_name: this.formApi.value.from_name,
-        to_name: "Muzammil",
-        from_email: this.formApi.value.from_email,
-        message: this.formApi.value.message,
-        });
-        if(response.status == 200 && response.text == 'OK'){
-        Swal.fire({
-          title:"Thank You",
-          text: "Got your message! I'll get back to you soon.",
-          icon: 'success',
-          confirmButtonColor:'#2778c4',
-          confirmButtonText: 'OK',
-          customClass: {
-              popup: 'my-custom-swal',
-              title: 'my-custom-title',
-          }
+  async send() {
+    emailjs.init('kJfS8RR9dekvphXCy');
+    let response = await emailjs.send('service_pwe1eny', 'template_r7ay4xe', {
+      from_name: this.formApi.value.from_name,
+      to_name: 'Muzammil',
+      from_email: this.formApi.value.from_email,
+      message: this.formApi.value.message,
+    });
+    if (response.status == 200 && response.text == 'OK') {
+      Swal.fire({
+        title: 'Thank You',
+        text: "Got your message! I'll get back to you soon.",
+        icon: 'success',
+        confirmButtonColor: '#2778c4',
+        confirmButtonText: 'OK',
+        customClass: {
+          popup: 'my-custom-swal',
+          title: 'my-custom-title',
+        },
       });
-      }
-      this.formApi.reset();
+    }
+    this.formApi.reset();
   }
 
+  //onscroll change nav bar bg
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const navbar = document.querySelector('.navbar') as HTMLElement;
+    const navbartoggler = document.querySelector(
+      '.navbar-toggler'
+    ) as HTMLElement;
+    const scrollPosition = window.pageYOffset;
+    const navbarHeight = navbar.offsetHeight;
 
-
-//onscroll change nav bar bg
-@HostListener('window:scroll', [])
-onWindowScroll() {
-  const navbar = document.querySelector('.navbar') as HTMLElement;
-  const navbartoggler = document.querySelector('.navbar-toggler') as HTMLElement;
-  const scrollPosition = window.pageYOffset;
-  const navbarHeight = navbar.offsetHeight;
-
-  if (scrollPosition > navbarHeight) {
-    navbar.classList.add('scrolled');
-    navbartoggler.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-    navbartoggler.classList.remove('scrolled');
+    if (scrollPosition > navbarHeight) {
+      navbar.classList.add('scrolled');
+      navbartoggler.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+      navbartoggler.classList.remove('scrolled');
+    }
   }
-}
 
-scrollToContainer(container:string){
-  const element = document.getElementById(container);
-  if (element) {
-    element.style.marginTop = '-50px';
-    element.scrollIntoView({ behavior: "smooth" });
-    element.style.marginTop = '0';
+  scrollToContainer(container: string) {
+    const element = document.getElementById(container);
+    if (element) {
+      element.style.marginTop = '-50px';
+      element.scrollIntoView({ behavior: 'smooth' });
+      element.style.marginTop = '0';
+    }
   }
-}
-
-
 }
